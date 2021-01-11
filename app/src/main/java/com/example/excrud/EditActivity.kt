cbcec883c526894f1596746a234e5d09fdea01e4
Sooyeon Jo <supop1009@gmail.com>
2021-01-11 23:36:12 +0900

    feat: Change init memo data method in MainActivity
    MainActivity의 메모 데이터 불러오는 작업을 Firestore 데이터 읽기 코드로 변경
    EditActivity의 updateMemo()의 memo data에 bookmark 값 추가

--- app/src/main/java/com/example/excrud/EditActivity.kt
+++ app/src/main/java/com/example/excrud/EditActivity.kt
@@ -2,8 +2,6 @@ package com.example.excrud
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
 import android.util.Log
 import android.view.MenuItem
 import com.example.excrud.databinding.ActivityEditBinding
@@ -11,9 +9,9 @@ import com.google.firebase.firestore.FirebaseFirestore
 import java.text.SimpleDateFormat
 import java.util.*
 
-class EditActivity : AppCompatActivity(), TextWatcher {
+class EditActivity : AppCompatActivity(){
     companion object {
-        private val TAG = "EditActivity"
+        private const val TAG = "EditActivity"
     }
 
     private lateinit var binding: ActivityEditBinding
@@ -22,15 +20,16 @@ class EditActivity : AppCompatActivity(), TextWatcher {
         super.onCreate(savedInstanceState)
         binding = ActivityEditBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        setSupportActionBar(binding.toolbar)
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        initView()
 
         //TODO create 모드일 때 toolbar menu icon save로 세팅,
         // edit 모드일 때 DB에서 정보 가져오고(READ) toolbar 아이콘 delete로 세팅
-        updateDate()
+        if (intent.hasExtra("content")) {
+            initMemo()
+        } else {
+            updateDate()
+        }
 
-        binding.contentEditText.addTextChangedListener(this)
     }
 
     override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
@@ -38,6 +37,10 @@ class EditActivity : AppCompatActivity(), TextWatcher {
             //TODO 입력한 메모 정보 DB에 저장하는 코드 넣기
             updateMemo()
             setVisible(false)
+            true
+        }
+        R.id.action_export -> {
+            //TODO 공유하는 bottomSheet 추가
             true
         }
         R.id.action_delete -> {
@@ -49,6 +52,24 @@ class EditActivity : AppCompatActivity(), TextWatcher {
         }
     }
 
+    private fun initView() {
+        initToolbar()
+    }
+
+    private fun initToolbar() {
+        setSupportActionBar(binding.toolbar)
+
+        supportActionBar?.apply {
+            setDisplayHomeAsUpEnabled(false)
+            setDisplayShowTitleEnabled(false)
+        }
+    }
+
+    private fun initMemo() {
+        binding.contentEditText.setText(intent.getStringExtra("content"))
+        binding.dateText.text = intent.getStringExtra("date")
+    }
+
     private fun updateDate() {
         val currentTime = Calendar.getInstance().time
         val date = SimpleDateFormat("yyyy년 MM월 dd일 HH:mm", Locale.KOREA).format(currentTime)
@@ -58,13 +79,14 @@ class EditActivity : AppCompatActivity(), TextWatcher {
 
     private fun updateMemo() {
         val db = FirebaseFirestore.getInstance()
-        val data = hashMapOf(
-            "content" to binding.contentEditText.text,
-            "date" to binding.dateText.text
-        )
+        val data = Memo().apply{
+            content = binding.contentEditText.text.toString()
+            date = binding.dateText.text.toString()
+            bookmark = false
+        }
 
         db.collection("memos")
-            .document("user1")
+            .document("${intent.getIntExtra("size", 4444)}")
             .set({ data })
             .addOnSuccessListener {
                 Log.d(TAG, "DocumentSnapshot added")
@@ -74,15 +96,8 @@ class EditActivity : AppCompatActivity(), TextWatcher {
             }
     }
 
-    override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-
-    }
-
-    override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-        updateDate()
-    }
-
-    override fun afterTextChanged(p0: Editable?) {
-
+    override fun onBackPressed() {
+        super.onBackPressed()
+        updateMemo()
     }
 }
--- app/src/main/java/com/example/excrud/MainActivity.kt
+++ app/src/main/java/com/example/excrud/MainActivity.kt
@@ -3,39 +3,39 @@ package com.example.excrud
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.excrud.databinding.ActivityMainBinding
 import com.google.firebase.firestore.FirebaseFirestore
-import java.text.SimpleDateFormat
-import java.util.*
+import com.google.firebase.firestore.ktx.toObject
 import kotlin.collections.ArrayList
 
 class MainActivity : AppCompatActivity() {
     companion object {
-        private val TAG = "MainActivity"
+        private const val TAG = "MainActivity"
     }
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var memoViewManager: RecyclerView.LayoutManager
-    private var memoList : ArrayList<Memo> = arrayListOf()
+    private var memoList: ArrayList<Memo> = arrayListOf()
+    private lateinit var memoAdapter: MemoAdapter
+    private val db = FirebaseFirestore.getInstance()
+    private val docRef = db.collection("memos")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        // TODO 리싸이클러뷰 클릭 리스너 추가 & 누르면 메모 확장하고 수정 가능하게 만듦. 삭제 버튼도 안에 넣기
-
-        val db = FirebaseFirestore.getInstance()
-
         initView()
     }
 
-    private fun initView(){
+    private fun initView() {
         initToolbar()
         initRecyclerView()
+        initMemoData()
     }
 
     private fun initToolbar() {
@@ -49,38 +49,37 @@ class MainActivity : AppCompatActivity() {
 
     private fun initRecyclerView() {
         memoViewManager = LinearLayoutManager(this)
+        memoAdapter = MemoAdapter(memoList) { memo -> adapterOnClick(memo) }
         binding.memoRecyclerview.apply {
             layoutManager = memoViewManager
-            adapter = MemoAdapter(initMemoData()) { memo -> adapterOnClick(memo) }
+            adapter = memoAdapter
             setHasFixedSize(true)
             addItemDecoration(DividerItemDecoration(context, R.drawable.line_divider, 20))
         }
     }
 
     private fun adapterOnClick(memo: Memo) {
+        //TODO memo position 가져와서 size에 putExtra 하기
         val intent = Intent(this, EditActivity()::class.java)
         intent.putExtra("content", memo.content)
         intent.putExtra("date", memo.date)
+        intent.putExtra("size", memoList.size)
         startActivity(intent)
     }
 
-    private fun getDate() : String{
-        val currentTime = Calendar.getInstance().time
-        return SimpleDateFormat("yyyy.MM.dd", Locale.KOREA).format(currentTime)
-    }
-
-    private fun initMemoData() : ArrayList<Memo>{
-        if(memoList.size == 0){
-            val memo = Memo().apply {
-                content = "내용 블라블라 안녕하세요 내용 블라블라 안녕하세요 내용 블라블라 안녕하세요 내용 블라블라 안녕하세요 내용 블라블라 안녕하세요"
-                date = getDate()
+    private fun initMemoData() {
+        memoList.clear()
+        docRef.get()
+            .addOnSuccessListener { result ->
+                for (document in result) {
+                    val memo = document.toObject<Memo>()
+                    memoList.add(memo)
+                }
+                memoAdapter.notifyDataSetChanged()
             }
-
-            for(i in 1..20){
-                memoList.add(memo)
+            .addOnFailureListener { exception ->
+                Log.d(TAG, "Error getting documents: ", exception)
             }
-        }
-        return memoList
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
